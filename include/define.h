444e454b713db5229d8c27895afac36700be5f45
TakuroIkeda <ikedatakuro0318@gmail.com>
2024-11-14 19:43:38 +0900

    実行した後

--- include/define.h
+++ include/define.h
@@ -1,14 +1,15 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
-#define server_num 4
+// パラメータ類
+#define server_num 3
 #define TIME 10000
-#define lambda 1
-#define node_num 10300
+#define lambda 0.1
+#define node_num 1300
 #define mu 0.05
 #define dt 0.1
 #define seed 1101
-#define roop_count 100
+#define roop_count 10
 
 typedef struct
 {
